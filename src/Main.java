b70ff8c495dfb78c8413cb3bb13df0940402fb6d
zabiazimi <zabiazimi1994@gmail.com>
2023-05-31 16:19:41 -0400

    First commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+
+        System.out.println("Hello world!");
+
+
+                for (int i = 0; i < 10; i++) {
+                    System.out.println("Hello world!");
+                    Thread.sleep(2000);
+
+                }
+
+            }
+        }
+
+
